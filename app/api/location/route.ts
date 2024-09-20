e2531cd210909050990e793b94b2730f7e49896f
shreyas20003 <shreyasas2975@gmail.com>
2024-09-20 12:58:12 +0530

    extra

---
+++ app/api/location/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import geoip from "geoip-lite";
+
+export async function GET(req: NextRequest) {
+    const forwardedFor = req.headers.get('x-forwarded-for');
+    const clientIp = forwardedFor ? forwardedFor.split(',')[0].trim() : req.ip;
+    // const ip = forwardedFor?.split(',')[0].trim();
+    // const geo = ip ? geoip.lookup(ip) : null;
+
+    // console.log(geo); // Log geo information for debugging
+
+    return NextResponse.json({
+        forwardedFor,
+        clientIp
+    });
+}
