15905ad2f7ca8e55bdb15c02ca71c91affe72e94
shreyas20003 <shreyasas2975@gmail.com>
2024-09-20 13:10:23 +0530

    extra

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -5,10 +5,11 @@ export async function GET(req: NextRequest) {
     const forwardedFor = req.headers.get('x-forwarded-for');
     const clientIp =  req.ip;
     const ip = forwardedFor?.split(',')[0].trim();
-    const geo = ip ? geoip.lookup(ip) : null;
+    // const geo = ip ? geoip.lookup(ip) : null;
     return NextResponse.json({
         forwardedFor,
         clientIp,
-        geo
+        ip
+        // geo
     });
 }
